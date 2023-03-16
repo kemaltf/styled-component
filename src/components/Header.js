c3711fa208a6573b3363d90bb5916cc4ed502541
kemal <kemaltaufikf@gmail.com>
2023-03-16 10:35:08 +0800

    create header styled component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
+import { StyledHeader } from "./styles/Header.styled";
 export default function Header() {
   return (
-    <header>
+    <StyledHeader bg="red">
       <h1>Hubble</h1>
-    </header>
+    </StyledHeader>
   );
 }
