5669dad61604126bb78e9a11f3896a66b82c0852
kemal <kemaltaufikf@gmail.com>
2023-03-16 10:05:52 +0800

    first styled component (container)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Header from "./components/Header";
+import { Container } from "./components/styles/Container.styled";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    </>
+  );
 }
 
 export default App;
