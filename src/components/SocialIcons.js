2f027c15de0f0c9aeb3ae90c0932628b98f73529
kemal <kemaltaufikf@gmail.com>
2023-03-17 16:13:57 +0800

    created styled-component for social media components

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,18 +1,23 @@
 import { FaTwitter, FaFacebook, FaLinkedin } from "react-icons/fa";
+import { StyledSocialIcons } from "./styles/SocialIcons.styled";
 export default function SocialIcons() {
   return (
-    <div>
+    <StyledSocialIcons>
       <li>
         <a href="https://twitter.com">
           <FaTwitter></FaTwitter>
         </a>
+      </li>
+      <li>
         <a href="https://twitter.com">
           <FaFacebook></FaFacebook>
         </a>
+      </li>
+      <li>
         <a href="https://twitter.com">
           <FaLinkedin></FaLinkedin>
         </a>
       </li>
-    </div>
+    </StyledSocialIcons>
   );
 }
