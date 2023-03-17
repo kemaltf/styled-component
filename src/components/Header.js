af660f31900f183a94109adf9b6200e30dec3344
kemal <kemaltaufikf@gmail.com>
2023-03-17 14:31:35 +0800

    Header styled component and flex styled component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,21 @@
-import { StyledHeader } from "./styles/Header.styled";
+import { Button } from "./styles/Button.styled";
+import { Container } from "./styles/Container.styled";
+import { StyledHeader, Nav, Logo } from "./styles/Header.styled";
+import { Flex } from "./styles/Flex.styled";
 export default function Header() {
   return (
     <StyledHeader>
-      <h1>Hubble</h1>
+      <Container>
+        <Nav>
+          <Logo src="./images/logo.svg"></Logo>
+          <Button>Try it free!</Button>
+        </Nav>
+        <Flex>
+          <div>
+            <h1>Build the community your fans will love</h1>
+          </div>
+        </Flex>
+      </Container>
     </StyledHeader>
   );
 }
